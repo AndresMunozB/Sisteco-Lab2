a2cf7701a6d6d309d9b3ff63e37490be0876547d
Andrés Muñoz <andres.munoz@symbiose.cl>
2019-01-06 19:07:02 -0300

    Funcionando
    Signed-off-by: Andrés Muñoz <andres.munoz@symbiose.cl>

--- cesar.py
+++ cesar.py
@@ -4,31 +4,26 @@
 
 class Cesar():
     def __init__(self):
-        self.password = None
-        self.text = None
+
         self.alphabet = ['!', ',', 'l', "'", 'T', 'B', 'F', '0', 'u', 'j', 'z', 'b', 'A', '#', 'n', '@', 'm', 'L', 'g', '[', '4', 'R', 'a', '|', 'v', 'c', 'U', ';', '~', 'S', '9', '5', 'o', 'k', ')', ' ', 'C', '+', '&', 'q', 'M', 'i', 'Q', '>', 'X', '/', '7', 'J', '-', 'E', 'H', 'w', 'V', 'K', '$', 't', '{', 'Y', '*', '%', 'e', '<', '}', '`', 'G', 'r', 'd', 's', '1', 'I', '=', 'W', '?', '_', ':', '^', '2', '3', '8', '6', '(', 'y', '.', 'Z', '\\', 'p', '"', 'D', 'O', 'N', 'x', ']', 'h', 'P', 'f']
     
-    def decrypt(self):
+    def decrypt(self,text, password):
         result = ""
-        for c in self.text:
-            result += self.alphabet[(self.alphabet.index(c) - self.password) % len(self.alphabet)]
+        for c in text:
+            result += self.alphabet[(self.alphabet.index(c) - password) % len(self.alphabet)]
         return result
 
-    def encrypt(self):
+    def encrypt(self,text, password):
         result = ""
-        for c in self.text:
-            result += self.alphabet[(self.alphabet.index(c) + self.password) % len(self.alphabet)]
+        for c in text:
+            result += self.alphabet[(self.alphabet.index(c) + password) % len(self.alphabet)]
         return result
     
     
-"""   
+"""
 cesar = Cesar()
-cesar.password = 20
-cesar.text = "hola como EStas!"
-
-encriptado = cesar.encrypt()
+encriptado = cesar.encrypt("hola como EStas!", 20)
 print (encriptado)
-cesar.text = encriptado
-desencriptado = cesar.decrypt()
+desencriptado = cesar.decrypt(encriptado,20)
 print (desencriptado)
 """
--- jad.py
+++ jad.py
@@ -34,23 +34,43 @@ class Jad():
         self.keys = list()
         self.cesar = Cesar()
 
-    def generatePasswordCesar(self, iteration):
-        value = ord(self.password[3])
+    def generatePasswordCesar(self, iteration,password):
+        value = ord(password[3])
         value = int ( (iteration + value) ** (iteration+value) )
         return  value
     
-    def generateKeys(self):
+    def generateKeys(self,password):
         self.keys = []
         for i in range(16):
-            self.cesar.text = self.password
-            self.cesar.password = self.generatePasswordCesar(i)
-            key = string_to_bit_array(self.cesar.encrypt())
+            key = string_to_bit_array(self.cesar.encrypt(password,self.generatePasswordCesar(i,password)))
             self.keys.append(key)
     
     def xor(self, t1, t2):#Apply a xor and return the resulting list
         return [x^y for x,y in zip(t1,t2)]
+
+    def feistel(self,size_block,action,text):
+
+        text_blocks = nsplit(text, size_block) #Se divide el texto en bloques de 8 bytes es decir 64 bits
+        #print(text_blocks)
         
-    def run(self, key, text,action=ENCRYPT, padding=False):
+        result = list()
+        for block in text_blocks:#Se aplica el método para cada bloque
+            block = string_to_bit_array(block)#Se convierte el bloque a binario 
+            g, d = nsplit(block, int(len(block)/2)) #g(LEFT), d(RIGHT)
+
+            tmp = None
+            for i in range(16): #Do the 16 rounds
+                if action == ENCRYPT:
+                    tmp = self.xor(self.keys[i], d)#If encrypt use Ki
+                else:
+                    tmp = self.xor(self.keys[15-i], d)#If decrypt start by the last key
+                tmp = self.xor(g, tmp)
+                g = d
+                d = tmp
+            result += d+g
+        return  bit_array_to_string(result)
+
+    """def run(self, key, text,action=ENCRYPT, padding=False):
         if len(key) < 8:
             raise "Key Should be 8 bytes long"
         elif len(key) > 8:
@@ -59,54 +79,37 @@ class Jad():
         self.password = key
         self.text = text
         self.generateKeys() #Se generan las claves para cada una de las iteraciones.
-        
-        
+
+        result_text = ""
         if action==ENCRYPT:
-            self.cesar.password = ord(self.password[4])
-            self.cesar.text = self.text
-            self.text = self.cesar.encrypt()
-        print(self.text)
-        text_blocks = nsplit(self.text, 8) #Se divide el texto en bloques de 8 bytes es decir 64 bits
-        
-
-        
-
-
-        #print(text_blocks)
-        result = list()
-        for block in text_blocks:#Se aplica el método para cada bloque
-            block = string_to_bit_array(block)#Se convierte el bloque a binario 
-            g, d = nsplit(block, 32) #g(LEFT), d(RIGHT)
-
-            tmp = None
-            for i in range(16): #Do the 16 rounds
-                if action == ENCRYPT:
-                    tmp = self.xor(self.keys[i], d)#If encrypt use Ki
-                else:
-                    tmp = self.xor(self.keys[15-i], d)#If decrypt start by the last key
-                g = d
-                d = tmp
-            result += d+g
-        
-        final_res = bit_array_to_string(result)
-        print(final_res)
+            
+            #print(result_text)
 
         if action==DECRYPT:
-            self.cesar.password = ord(self.password[4])
-            self.cesar.text = final_res
-            final_res = self.cesar.decrypt()
-                
-        return final_res 
+            result_text = self.feistel(8,action,self.text)
+            result_text = self.cesar.decrypt(result_text,ord(self.password[4]))
+            #print(result_text)
+        
+        return result_text"""
+
+    def encrypt(self,text,password,size_block):
+        self.generateKeys(password)
+        result_text = self.cesar.encrypt(text,ord(password[4]))
+        result_text = self.feistel(size_block,ENCRYPT,result_text)
+        return result_text
+
+    def decrypt(self,text,password,size_block):
+        self.generateKeys(password)
+        result_text = self.feistel(size_block,DECRYPT,text)
+        result_text = self.cesar.decrypt(result_text,ord(password[4]))
+        return result_text
+
 
     
     
-
 jad = Jad()
-jad.password = "holacomo"
-jad.text = "hola como estas!"
-jad.generateKeys()
-textooo = jad.run("holacomo","hola como estas!",ENCRYPT)
-#print(textooo)
-textooo = jad.run("holacomo",textooo,DECRYPT)
-#print(textooo)
+textooo = jad.encrypt("hola como estas!","holacomo",2)
+print(textooo)
+textooo = jad.decrypt(textooo,"holacomo",2)
+print(textooo)
 #print(jad.keys)
